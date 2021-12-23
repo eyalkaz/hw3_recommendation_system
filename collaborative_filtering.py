8090a3370e42006ea322df9001d5951c529acfe0
eyalkaz <eyalkazula@gmail.com>
2021-12-23 22:32:24 +0200

    refactor and fake user

--- collaborative_filtering.py
+++ collaborative_filtering.py
@@ -18,11 +18,37 @@ class collaborative_filtering:
         self.movie_titles = {}
     def create_fake_user(self,rating):
         "*** YOUR CODE HERE ***"
-        return rating
+        self.calc_method_matrix(rating)
+        self.user_item_matrix.loc[283238] = 0
+        self.mean_user_rating = np.append(self.mean_user_rating, [0])
+        self.mean_user_rating = self.mean_user_rating.reshape(self.mean_user_rating.shape[0], 1)
+        self.calc_matrix_similarity()
+        return self.predict_movies(283238, 5)
 
     def create_movies_title_dict(self, df):
         self.movie_titles = dict(zip(df.movieId, df.title))
 
+    def calc_method_matrix(self, ratings, is_user=True):
+        if is_user:
+            self.user_item_matrix = ratings.pivot(index='userId', columns='movieId', values='rating')
+            self.mean_user_rating = self.user_item_matrix.mean(axis=1).to_numpy().reshape(-1, 1)
+            self.user_item_matrix = self.user_item_matrix.sub(self.user_item_matrix.mean(axis=1), axis=0).fillna(0)
+            self.movies_rating_matrix = self.user_item_matrix.T
+        else:
+            self.movies_rating_matrix = ratings.pivot(index='movieId', columns='userId', values='rating')
+            self.mean_user_rating = self.movies_rating_matrix.T.mean(axis=1).to_numpy().reshape(-1, 1)
+            # we take the mean of the columns now because we need mean of users
+            self.movies_rating_matrix = self.movies_rating_matrix.sub(self.movies_rating_matrix.mean(axis=0),
+                                                                      axis=1).fillna(0)
+            self.user_item_matrix = self.movies_rating_matrix.T
+
+    def calc_matrix_similarity(self, is_user=True):
+        if is_user:
+            sim_matrix = 1 - pairwise_distances(self.user_item_matrix, metric='cosine')  # cosine similarity = 1 - cosiine distance
+            self.user_based_matrix = sim_matrix
+        else:
+            sim_matrix = 1 - pairwise_distances(self.movies_rating_matrix, metric='cosine')  # cosine similarity = 1 - cosiine distance
+            self.item_based_metrix = sim_matrix
 
     def create_user_based_matrix(self, data):
         ratings = data[0]
@@ -32,29 +58,15 @@ class collaborative_filtering:
 
         "*** YOUR CODE HERE ***"
         self.create_movies_title_dict(data[1])
-        self.user_item_matrix = ratings.pivot(index='userId', columns='movieId', values='rating')
-        self.mean_user_rating = self.user_item_matrix.mean(axis=1).to_numpy().reshape(-1, 1)
-        self.user_item_matrix = self.user_item_matrix.sub(self.user_item_matrix.mean(axis=1), axis=0).fillna(0)
-        self.user_based_matrix = 1 - pairwise_distances(self.user_item_matrix, metric='cosine')  # cosine similarity = 1 - cosiine distance
-
-        # for predilection
-        if not self.movies_rating_matrix:  # movies rating matrix not created yet
-            self.movies_rating_matrix = self.user_item_matrix.T
+        self.calc_method_matrix(ratings, True)
+        self.calc_matrix_similarity(True)
 
     def create_item_based_matrix(self, data):
         "*** YOUR CODE HERE ***"
         ratings = data[0]
         self.create_movies_title_dict(data[1])
-        self.movies_rating_matrix = ratings.pivot(index='movieId', columns='userId', values='rating')
-        if not self.mean_user_rating:  # we didnt call user item matrix
-            self.mean_user_rating = self.movies_rating_matrix.T.mean(axis=1).to_numpy().reshape(-1, 1)
-        # we take the mean of the columns now because we need mean of users
-        self.movies_rating_matrix = self.movies_rating_matrix.sub(self.movies_rating_matrix.mean(axis=0), axis=1).fillna(0)
-        self.item_based_metrix = 1 - pairwise_distances(self.movies_rating_matrix, metric='cosine')  # cosine similarity = 1 - cosiine distance
-
-        # for predilection
-        if not self.user_item_matrix:  # user rating matrix not created yet
-            self.user_item_matrix = self.movies_rating_matrix.T
+        self.calc_method_matrix(ratings, False)
+        self.calc_matrix_similarity(False)
 
     def predict_movies(self, user_id, k, is_user_based=True):
         "*** YOUR CODE HERE ***"
@@ -64,7 +76,7 @@ class collaborative_filtering:
         if is_user_based:
             prediction_matrix = self.user_based_matrix.dot(self.user_item_matrix)
             denominator = np.array([np.abs(self.user_based_matrix).sum(axis=1)]).T
-            prediction_matrix = self.mean_user_rating + prediction_matrix / denominator
+            prediction_matrix = self.mean_user_rating + (prediction_matrix / denominator)
         else:
             prediction_matrix = self.item_based_metrix.dot(self.movies_rating_matrix)
             denominator = np.array([np.abs(self.item_based_metrix).sum(axis=1)]).T
