41361e851af15bd33e7b34ab2c19f7f1f10c2ef1
eyalkaz <eyalkazula@gmail.com>
2021-12-23 17:56:13 +0200

    Initial commit

---
+++ collaborative_filtering.py
@@ -0,0 +1,84 @@
+# Eyal Kazula 209133693
+import sys
+import pandas as pd
+import numpy as np
+import heapq
+from sklearn.metrics.pairwise import pairwise_distances
+
+
+class collaborative_filtering:
+    def __init__(self):
+        self.user_based_matrix = []
+        self.item_based_metrix = []
+        self.user_item_matrix = []
+        self.movies_rating_matrix = []
+        self.mean_user_rating = []
+        self.dict_users = {}
+        self.dict_items = {}
+        self.movie_titles = {}
+    def create_fake_user(self,rating):
+        "*** YOUR CODE HERE ***"
+        return rating
+
+    def create_movies_title_dict(self, df):
+        self.movie_titles = dict(zip(df.movieId, df.title))
+
+
+    def create_user_based_matrix(self, data):
+        ratings = data[0]
+
+        # for adding fake user
+        #ratings = self.create_fake_user(ratings)
+
+        "*** YOUR CODE HERE ***"
+        self.create_movies_title_dict(data[1])
+        self.user_item_matrix = ratings.pivot(index='userId', columns='movieId', values='rating')
+        self.mean_user_rating = self.user_item_matrix.mean(axis=1).to_numpy().reshape(-1, 1)
+        self.user_item_matrix = self.user_item_matrix.sub(self.user_item_matrix.mean(axis=1), axis=0).fillna(0)
+        self.user_based_matrix = 1 - pairwise_distances(self.user_item_matrix, metric='cosine')  # cosine similarity = 1 - cosiine distance
+
+        # for predilection
+        if not self.movies_rating_matrix:  # movies rating matrix not created yet
+            self.movies_rating_matrix = self.user_item_matrix.T
+
+    def create_item_based_matrix(self, data):
+        "*** YOUR CODE HERE ***"
+        ratings = data[0]
+        self.create_movies_title_dict(data[1])
+        self.movies_rating_matrix = ratings.pivot(index='movieId', columns='userId', values='rating')
+        if not self.mean_user_rating:  # we didnt call user item matrix
+            self.mean_user_rating = self.movies_rating_matrix.T.mean(axis=1).to_numpy().reshape(-1, 1)
+        # we take the mean of the columns now because we need mean of users
+        self.movies_rating_matrix = self.movies_rating_matrix.sub(self.movies_rating_matrix.mean(axis=0), axis=1).fillna(0)
+        self.item_based_metrix = 1 - pairwise_distances(self.movies_rating_matrix, metric='cosine')  # cosine similarity = 1 - cosiine distance
+
+        # for predilection
+        if not self.user_item_matrix:  # user rating matrix not created yet
+            self.user_item_matrix = self.movies_rating_matrix.T
+
+    def predict_movies(self, user_id, k, is_user_based=True):
+        "*** YOUR CODE HERE ***"
+        user_id = int(user_id)
+        self.dict_users = {k: v for v, k in enumerate(self.user_item_matrix.index)}  # flipped dictionary with user : index
+        self.dict_items = dict(enumerate(self.movies_rating_matrix.index))  # index : movie
+        if is_user_based:
+            prediction_matrix = self.user_based_matrix.dot(self.user_item_matrix)
+            denominator = np.array([np.abs(self.user_based_matrix).sum(axis=1)]).T
+            prediction_matrix = self.mean_user_rating + prediction_matrix / denominator
+        else:
+            prediction_matrix = self.item_based_metrix.dot(self.movies_rating_matrix)
+            denominator = np.array([np.abs(self.item_based_metrix).sum(axis=1)]).T
+            prediction_matrix = self.mean_user_rating + (prediction_matrix / denominator).T  # transpose so it predict for user
+
+        candidates = prediction_matrix[self.dict_users[user_id]]
+        already_rated = np.where(self.user_item_matrix.loc[[user_id]].values != 0)[1]
+        candidates[already_rated] = min(candidates) - 1  # never choose them
+        top_k = np.argpartition(candidates, -k)[-k:].tolist()
+
+        def my_func(x):
+            return candidates[x]
+        top_k.sort(reverse=True, key=my_func)
+        top_k_movies_id = [self.dict_items[i] for i in top_k]
+        top_k_movies_titles = [self.movie_titles[id] for id in top_k_movies_id]
+        return top_k_movies_titles
+
---
+++ data.py
@@ -0,0 +1,43 @@
+# Eyal Kazula 209133693
+import sys
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def watch_data_info(data):
+    for d in data:
+        # This function returns the first 5 rows for the object based on position.
+        # It is useful for quickly testing if your object has the right type of data in it.
+        print(d.head())
+
+        # This method prints information about a DataFrame including the index dtype and column dtypes, non-null values and memory usage.
+        print(d.info())
+
+        # Descriptive statistics include those that summarize the central tendency, dispersion and shape of a dataset’s distribution, excluding NaN values.
+        print(d.describe(include='all').transpose())
+
+
+def print_data(data):
+    "*** YOUR CODE HERE ***"
+    ratings = data[0]
+    # 1.1
+    print("unique users: ", ratings["userId"].nunique())
+    print("unique movies: ", ratings["movieId"].nunique())
+    print("number of ratings: ", ratings["userId"].count())
+    # 1.2
+    rating_per_movie = ratings["movieId"].value_counts()
+    print("max movie ratings:", rating_per_movie.max())
+    print("min movie ratings:", rating_per_movie.min())
+    # 1.3
+    ratings_per_user = ratings["userId"].value_counts()
+    print("max user ratings:", ratings_per_user.max())
+    print("min user ratings:", ratings_per_user.min())
+
+
+def plot_data(data, plot = True):
+    "*** YOUR CODE HERE ***"
+    # get mean rating of all movies
+    ratings = data[0]
+    grades_series = ratings["rating"].value_counts().sort_index()
+    grades_series.plot.bar(logy=True, title="log scale rating")
+    if plot:
+        plt.show()
