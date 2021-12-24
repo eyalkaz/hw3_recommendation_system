a840c9504b41f4d3864eaa8b1d9a8c3b943abcc5
eyalkaz <eyalkazula@gmail.com>
2021-12-24 16:08:55 +0200

    refactor and groundwork for eval

--- collaborative_filtering.py
+++ collaborative_filtering.py
@@ -13,9 +13,13 @@ class collaborative_filtering:
         self.user_item_matrix = []
         self.movies_rating_matrix = []
         self.mean_user_rating = []
-        self.dict_users = {}
-        self.dict_items = {}
+        self.dict_users_indexes = {}
+        self.dict_indexes_users = {}
+        self.dict_indexes_movies = {}
+        self.dict_movies_indexes = {}
         self.movie_titles = {}
+        self.user_prediction_matrix = []
+        self.items_prediction_matrix = []
     def create_fake_user(self,rating):
         "*** YOUR CODE HERE ***"
         self.calc_method_matrix(rating)
@@ -42,6 +46,11 @@ class collaborative_filtering:
                                                                       axis=1).fillna(0)
             self.user_item_matrix = self.movies_rating_matrix.T
 
+        self.dict_users_indexes = {k: v for v, k in enumerate(self.user_item_matrix.index)}  # flipped dictionary with user : index
+        self.dict_indexes_users = dict(enumerate(self.user_item_matrix.index))
+        self.dict_movies_indexes = {k: v for v, k in enumerate(self.movies_rating_matrix.index)}
+        self.dict_indexes_movies = dict(enumerate(self.movies_rating_matrix.index))  # index : movie
+
     def calc_matrix_similarity(self, is_user=True):
         if is_user:
             sim_matrix = 1 - pairwise_distances(self.user_item_matrix, metric='cosine')  # cosine similarity = 1 - cosiine distance
@@ -49,6 +58,18 @@ class collaborative_filtering:
         else:
             sim_matrix = 1 - pairwise_distances(self.movies_rating_matrix, metric='cosine')  # cosine similarity = 1 - cosiine distance
             self.item_based_metrix = sim_matrix
+
+    def create_prediction_matrix(self, is_user=True):
+        if is_user:
+            prediction_matrix = self.user_based_matrix.dot(self.user_item_matrix)
+            denominator = np.array([np.abs(self.user_based_matrix).sum(axis=1)]).T
+            prediction_matrix = self.mean_user_rating + (prediction_matrix / denominator)
+            self.user_prediction_matrix  = prediction_matrix
+        else:
+            prediction_matrix = self.item_based_metrix.dot(self.movies_rating_matrix)
+            denominator = np.array([np.abs(self.item_based_metrix).sum(axis=1)]).T
+            prediction_matrix = self.mean_user_rating + (prediction_matrix / denominator).T  # transpose so it predict for user
+            self.items_prediction_matrix = prediction_matrix
 
     def create_user_based_matrix(self, data):
         ratings = data[0]
@@ -60,6 +81,7 @@ class collaborative_filtering:
         self.create_movies_title_dict(data[1])
         self.calc_method_matrix(ratings, True)
         self.calc_matrix_similarity(True)
+        self.create_prediction_matrix(True)
 
     def create_item_based_matrix(self, data):
         "*** YOUR CODE HERE ***"
@@ -67,22 +89,17 @@ class collaborative_filtering:
         self.create_movies_title_dict(data[1])
         self.calc_method_matrix(ratings, False)
         self.calc_matrix_similarity(False)
+        self.create_prediction_matrix(False)
 
     def predict_movies(self, user_id, k, is_user_based=True):
         "*** YOUR CODE HERE ***"
-        user_id = int(user_id)
-        self.dict_users = {k: v for v, k in enumerate(self.user_item_matrix.index)}  # flipped dictionary with user : index
-        self.dict_items = dict(enumerate(self.movies_rating_matrix.index))  # index : movie
         if is_user_based:
-            prediction_matrix = self.user_based_matrix.dot(self.user_item_matrix)
-            denominator = np.array([np.abs(self.user_based_matrix).sum(axis=1)]).T
-            prediction_matrix = self.mean_user_rating + (prediction_matrix / denominator)
+            prediction_matrix = self.user_prediction_matrix
         else:
-            prediction_matrix = self.item_based_metrix.dot(self.movies_rating_matrix)
-            denominator = np.array([np.abs(self.item_based_metrix).sum(axis=1)]).T
-            prediction_matrix = self.mean_user_rating + (prediction_matrix / denominator).T  # transpose so it predict for user
+            prediction_matrix = self.items_prediction_matrix
 
-        candidates = prediction_matrix[self.dict_users[user_id]]
+        user_id = int(user_id)
+        candidates = prediction_matrix[self.dict_users_indexes[user_id]]
         already_rated = np.where(self.user_item_matrix.loc[[user_id]].values != 0)[1]
         candidates[already_rated] = min(candidates) - 1  # never choose them
         top_k = np.argpartition(candidates, -k)[-k:].tolist()
@@ -90,7 +107,7 @@ class collaborative_filtering:
         def my_func(x):
             return candidates[x]
         top_k.sort(reverse=True, key=my_func)
-        top_k_movies_id = [self.dict_items[i] for i in top_k]
+        top_k_movies_id = [self.dict_indexes_movies[i] for i in top_k]
         top_k_movies_titles = [self.movie_titles[id] for id in top_k_movies_id]
         return top_k_movies_titles
 
--- evaluation.py
+++ evaluation.py
@@ -1,4 +1,6 @@
 # Eyal Kazula 209133693
+import math
+
 from sklearn.metrics import mean_squared_error
 from math import sqrt
 # Import Pandas
@@ -16,7 +18,19 @@ def ARHA(test_set, cf, is_user_based = True):
 
 def RSME(test_set, cf, is_user_based = True):
     "*** YOUR CODE HERE ***"
-    val = 0  # this value should be changed.
+    numerator = 0
+    N = test_set.__len__()
+    test_user_item_matrix = test_set.pivot(index='userId', columns='movieId', values='rating').fillna(0)
+    if is_user_based:
+        prediction_matrix = cf.user_prediction_matrix
+    else:
+        prediction_matrix = cf.items_prediction_matrix
+    for user_id in test_set["userId"].unique():
+        for movie_id in get_rated_movies(user_id):
+            predicted_rating = prediction_matrix[cf.dict_users_indexes[user_id], cf.dict_movies_indexes[movie_id]]
+            actual_rating = test_user_item_matrix.loc[user_id].loc[movie_id]
+            numerator += (predicted_rating - actual_rating)**2
+    val = math.sqrt(numerator/N)
     print("RMSE: " + str(val))
 
 
